8df8f7464058554191ad58d4c6a06207548603e1
Pragadeeshwar <pragadeeshwar0801@gmail.com>
2022-03-15 18:42:41 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ module.exports = (app) => {
   app.log.info("Yay, the app was loaded!");
 
   app.on("push", async (context) => {
-
+	
+	const createIssue = context.issue({
+		body: "The repository has been updated",
+	});
+	return context.octokit.issues.create(createIssue);
 	app.log.info("context");
 	
   });
